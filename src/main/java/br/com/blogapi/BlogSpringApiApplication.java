a9630a5b6a609296512a42636c8f7c863ce4738a
claudio-teles <claudio_teles@yahoo.com>
2021-08-31 12:26:04 -0300

    Commit Um

---
+++ src/main/java/br/com/blogapi/BlogSpringApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.blogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSpringApiApplication.class, args);
+	}
+
+}
